c723da3ddd701ae2a54dc153e35826e8b5fec7c9
Oscar Alberto Ceballos Contreras <oscaralbertoceballos@gmail.com>
2020-10-23 00:42:30 -0400

    style(Added Icons and navlinks to Header): Styling Icons and navlinks in Header component

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,8 @@
 import React from "react";
 import "../styles/header.scss";
 import Link from "next/link";
-
+const people = "/img/people.svg";
+const jobs = "/img/jobs.svg";
 const Header = () => {
   return (
     <header>
@@ -10,6 +11,20 @@ const Header = () => {
           <h1>torre</h1>
         </a>
       </Link>
+      <div className="icons">
+        <Link href="/search/jobs">
+          <a>
+            <img src={jobs} alt="people" />
+            <h1>Search Jobs</h1>
+          </a>
+        </Link>
+        <Link href="/search/people">
+          <a>
+            <img src={people} alt="people" />
+            <h1>Search People</h1>
+          </a>
+        </Link>
+      </div>
     </header>
   );
 };
